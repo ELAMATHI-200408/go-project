03f88310a872fe556fd57ed3221f10ab102f7af7
ELAMATHI-200408 <elamathi200408@gmail.com>
2026-07-02 14:52:48 +0530

    Update 1_print.go

--- Basics/1_print.go
+++ Basics/1_print.go
@@ -1,1 +1,15 @@
-// Print "Hello, World" and your name using fmt.Println and fmt.Printf
+package main   // Print "Hello, World" and your name using fmt.Println and fmt.Printf
+//import package fmt for input/output
+import(
+  "fmt"
+)
+//main function
+func main(){
+  //initializes the name
+  //name := "Elamathi"
+  //print hello world
+  fmt.Println("Hello, World")
+  // print the name and string is used to convert the byte to string
+  //fmt.Println(string(name))
+  fmt.Printf("Elamathi")
+}
