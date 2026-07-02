2c1ac0fc211de8ac8e34e6d3086ddc87a9a8f455
ELAMATHI-200408 <elamathi200408@gmail.com>
2026-07-02 15:38:52 +0530

    Update 2_variable_declaration.go

--- Basics/2_variable_declaration.go
+++ Basics/2_variable_declaration.go
@@ -1,1 +1,27 @@
 // Declare an int, a float, a string, and a bool using both var and :=.
+package main
+import(
+  "fmt"
+  )
+func main(){
+ //int := 10
+ //float := 11.2
+ //name := "Vijay"
+  //bool := false
+ //fmt.Println(int)
+ //fmt.Println(float)
+ //fmt.Println(name)
+ //fmt.Println(bool)
+ var age int = 10
+ var accuracy float32 = 11.2
+ var name string = "vijay"
+ var valid bool = true
+ fmt.Println(age)
+ fmt.Println(accuracy)
+ fmt.Println(name)
+ fmt.Println(valid)
+}
+
+ 
+
+  
