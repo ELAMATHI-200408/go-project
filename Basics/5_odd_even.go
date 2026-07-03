d16518670cec0492ac916e9cdbcb189937b17a9b
SakthiKavin <sakthikavin.ss@thoughtworks.com>
2026-07-03 19:01:32 +0530

    Add exercises 5 and 6

---
+++ Basics/5_odd_even.go
@@ -0,0 +1,2 @@
+// Check if a number is even/odd, positive/negative/zero using `switch`
+// Get the number from user input
---
+++ Basics/6_looping.go
@@ -0,0 +1,39 @@
+// Six problems
+// 1. Print this
+// *
+// *
+// *
+
+// 2. Print this
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+
+// 3. Print this
+// *
+// * *
+// * * *
+// * * * *
+
+// 4. Print this.. if odd print 1, if even print 0
+// 1
+// 0 0
+// 1 1 1
+// 0 0 0 0
+
+// 5. Print this..print only odd rows / even rows
+// 1
+// 3 3 3
+// 5 5 5 5 5
+
+// or
+
+// 2 2
+// 4 4 4 4
+
+// 6. Print this
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
