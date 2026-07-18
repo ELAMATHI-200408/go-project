41645a6e3d060e8f26bd5d5ad91f518a7c5dec42
ELAMATHI-200408 <elamathi200408@gmail.com>
2026-07-18 11:22:06 +0530

    loop concepts

--- Basics/6_looping.go
+++ Basics/6_looping.go
@@ -6,9 +6,9 @@
 //package main
 //import "fmt"
 //func main(){
-	//for i := 0; i<3; i++{
-	//	fmt.Println("*")
-	//}
+//for i := 0; i<3; i++{
+//	fmt.Println("*")
+//}
 
 //}
 
@@ -17,31 +17,55 @@
 // 2 2
 // 3 3 3
 // 4 4 4 4
-package main
-import "fmt"
-func main(){
-	for i := 1; i<5; i++{
-		//for j :=2; j<2; j++{
-			fmt.Println(i*1)
-			//fmt.Println(j*1)
-		}
-	}
+//package main
+//import "fmt"
+//func main(){
+//for i := 1; i <= 4; i++ {
+//for j := 1; j <= i; j++ {
+//	fmt.Print(i," ")
+//}
+//fmt.Println()
+//}
 
-}
-
-
+//}
 
 // 3. Print this
 // *
 // * *
 // * * *
 // * * * *
+//package main
+//import "fmt"
+//func main(){
+//for i := 1; i <= 4; i++ {
+//for j := 1; j <= i; j++ {
+//fmt.Print("*" ," ")
+//}
+// fmt.Println()
+//}
+
+//}
 
 // 4. Print this.. if odd print 1, if even print 0
 // 1
 // 0 0
 // 1 1 1
 // 0 0 0 0
+//package main
+//import "fmt"
+//func main(){
+//for i := 1; i <= 4; i++ {
+//for j := 1; j <= i; j++ {
+//if (i%2==0){
+//fmt.Print("0"," ")
+//}else{
+//fmt.Print("1"," ")
+//}
+// }
+// fmt.Println()
+//}
+
+//}
 
 // 5. Print this..print only odd rows / even rows
 // 1
@@ -52,9 +76,39 @@ func main(){
 
 // 2 2
 // 4 4 4 4
+//package main
+//import "fmt"
+//func main(){
+//for i := 1; i <= 6; i++ {
+//for j := 1; j <=i; j++ {
+//if (i%2==0){
+//continue
+// }else{
+//fmt.Print(i," ")
+//}
+//}
+//fmt.Println()
+//}
+
+//}
 
 // 6. Print this
 // 1
 // 2 3
 // 4 5 6
 // 7 8 9 10
+package main
+
+import "fmt"
+
+func main() {
+	num := 1
+	for i := 1; i <= 4; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print(num, " ")
+			num++
+		}
+		fmt.Println()
+	}
+
+}
